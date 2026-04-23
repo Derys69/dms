46d9da01e636cf69b82efb3b294a9b262a70578f
Derys69 <ddharmawan03@studen.ciputra.ac.id>
2026-04-24 07:42:19 +0900

    up semua file

--- create_db.go
+++ create_db.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// Koneksi ke db bawaan 'postgres' untuk membuat database baru
-	dsn := "host=localhost user=postgres password=123456 dbname=postgres port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Ambil data dari .env isinya sesuai dengan db di pc
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PORT"),
+	)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	// Langsung eksekusi buat database
-	db.Exec("CREATE DATABASE server_db;")
+	dbName := os.Getenv("DB_NAME")
+	createQuery := fmt.Sprintf("CREATE DATABASE %s;", dbName)
+
+	if err := db.Exec(createQuery).Error; err != nil {
+		log.Println("Database mungkin sudah ada atau gagal dibuat:", err)
+	} else {
+		log.Println("Database", dbName, "berhasil dibuat!")
+	}
 }
--- internal/config/db.go
+++ internal/config/db.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,7 +12,14 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=postgres password=123456 dbname=server_db port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	// data dari .env
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
